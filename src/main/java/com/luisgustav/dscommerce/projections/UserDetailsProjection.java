051323180af1d1efbc87a1874a42b4e33d60c714
Luis Gustavo <gustavdev28@outlook.com>
2023-12-13 10:40:31 -0300

    UserDetailsService

---
+++ src/main/java/com/luisgustav/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.luisgustav.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
